4a3e84a3bc5e31d5f37734b8c65bc0f195bc77b1
mutz3l <mutz3l@gmail.com>
2023-02-17 12:14:20 +0100

    added own folder for keyboard tests

---
+++ keyboards/l3ztum/lotus58/config.h
@@ -0,0 +1,30 @@
+// Copyright 2022 Markus Knutsson (@TweetyDaBird)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#include "config_common.h"
+
+// Not working correct in json implementation
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
+#define SPLIT_USB_TIMEOUT_POLL 10
+
+// Not yet supported in json
+
+// Bootmagic Lite key configuration
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+#define BOOTMAGIC_LITE_ROW_RIGHT 5
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 0
+
+#define SPLIT_HAND_PIN B5
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_OLED_ENABLE
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET              // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define WS2812_PIO_USE_PIO1
+#define SERIAL_PIO_USE_PIO1
+
+#define SERIAL_USART_TX_PIN C7
