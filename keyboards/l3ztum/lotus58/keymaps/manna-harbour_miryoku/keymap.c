ea04cf4ff69bd4f056b7b081df786b08f1bc3f8b
mutz3l <mutz3l@gmail.com>
2023-05-04 13:13:06 +0200

    merging mouse and keyboard part1

--- keyboards/l3ztum/lotus58/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/l3ztum/lotus58/keymaps/manna-harbour_miryoku/keymap.c
@@ -1,5 +1,20 @@
 // Copyright 2019 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#include QMK_KEYBOARD_H
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP)  },//base
+    [1] =  { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP),},//extra
+    [2] =  { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP),},//tap
+    [3] = { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP), },//button
+    [4] = { ENCODER_CCW_CW(RGB_SPD, RGB_SPI), },//nav
+    [5] = { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP), },//mouse
+    [6] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), },//media
+    [7] = { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP), },//num
+    [8] = { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP), },//sys
+    [9] = { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP), }//fun
+};
+#endif
--- keyboards/l3ztum/lotus58/rgb_full/rules.mk
+++ keyboards/l3ztum/lotus58/rgb_full/rules.mk
@@ -5,3 +5,9 @@ WPM_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 UNICODE_ENABLE = yes
 COMBO_ENABLE = yes
+
+PS2_MOUSE_ENABLE = yes
+PS2_USE_WAIT = yes
+PS2_ENABLE = yes
+
+PS2_DRIVER = interrupt
