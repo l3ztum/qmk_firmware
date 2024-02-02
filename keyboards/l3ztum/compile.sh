169a9006d804e116ebbd7b9e0ef8bec9f0b61101
mutz3l <mutz3l@gmail.com>
2024-02-02 19:29:29 +0100

    branch fixing part2

--- keyboards/l3ztum/compile.sh
+++ keyboards/l3ztum/compile.sh
@@ -1,2 +1,2 @@
-qmk compile -kb l3ztum/lotus58/rgb_full -km default -e CONVERT_TO=promicro_rp2040 -j 16
-qmk compile -kb l3ztum/lotus58/rgb_full -km default -e CONVERT_TO=kb2040 -j 16
+qmk compile -c -kb l3ztum -km manna-harbour_miryoku -e MIRYOKU_ALPHAS=QWERTZ -e MIRYOKU_CLIPBOARD=WIN -e MIRYOKU_NAV=INVERTEDT -e MIRYOKU_LAYERS=FLIP -e CONVERT_TO=promicro_rp2040 -j 16
+qmk compile -c -kb l3ztum -km manna-harbour_miryoku -e MIRYOKU_ALPHAS=QWERTZ -e MIRYOKU_CLIPBOARD=WIN -e MIRYOKU_NAV=INVERTEDT -e MIRYOKU_LAYERS=FLIP -e CONVERT_TO=kb2040 -j 16
