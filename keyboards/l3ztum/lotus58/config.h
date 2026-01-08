62cbf570dc6707fb4d62388fbbd4adb8ad41acdc
mutz3l <mutz3l@gmail.com>
2026-01-08 23:57:52 +0100

    added missing local changes

--- keyboards/l3ztum/lotus58/config.h
+++ keyboards/l3ztum/lotus58/config.h
@@ -8,7 +8,7 @@
 #define RGB_MATRIX_LED_COUNT RGBLED_NUM
 
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 175
-#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#define RGB_MATRIX_SLEEP // turn off effects when suspended
 
 #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
@@ -41,7 +41,7 @@
 
 // Not yet supported in json
 
-#define SPLIT_HAND_PIN B5
+// #define SPLIT_HAND_PIN B5
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
 #define SPLIT_OLED_ENABLE
